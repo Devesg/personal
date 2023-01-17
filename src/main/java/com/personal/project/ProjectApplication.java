b05278ad819dad412aa22e451c3cf9d2a8e2595b
Devesg <e_s_g@naver.com>
2023-01-18 02:03:56 +0900

    :tada: Personal Project Setting

---
+++ src/main/java/com/personal/project/ProjectApplication.java
@@ -0,0 +1,14 @@
+package com.personal.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("12");
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
